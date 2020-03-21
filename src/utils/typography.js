8ba823563d3c93d47161b59e671f53fe6321ad7a
jhamPac <cloud@kevia.me>
2020-03-21 15:52:00 -0700

    typography working

---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from "typography"
+
+const t = new Typography({
+    baseFontSize: "16px",
+    headerFontFamily: ["Share Tech Mono", "monospace"],
+    bodyFontFamily: ["Share Tech Mono", "monospace"],
+})
+
+export default t

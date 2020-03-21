9167e02e0f902f579bc21299a3dc34eeeba2d3da
jhamPac <cloud@kevia.me>
2020-03-20 17:33:41 -0700

    removed starter files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => <di>British Racing Green</di>
-
-export default IndexPage
+export default function LandingPage(props) {
+    return <div>British Racing Green</div>
+}

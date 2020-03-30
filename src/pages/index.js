74321a25616e6eafd495dace415e6ea97d075585
jhamPac <cloud@kevia.me>
2020-03-29 19:20:58 -0700

    renamed LandingPage to X

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Container from "../components/container"
 import Header from "../components/header"
 
-export default function LandingPage(props) {
+export default function X(props) {
     return (
         <Container>
             <Header>

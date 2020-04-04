3ce9db97d9b21a7641921158c8a5505160ccb79e
jhamPac <cloud@kevia.me>
2020-04-03 21:18:10 -0700

    yeah going with this route

--- src/components/container.js
+++ src/components/container.js
@@ -1,39 +1,6 @@
 import React from "react"
-import styled from "styled-components"
+import Grid from "@material-ui/core/Grid"
 
 export default function Container(props) {
-    return <C>{props.children}</C>
+    return <Grid {...props} />
 }
-
-const C = styled.div`
-    height: 100vh;
-    width: 100vw;
-    padding: 15px;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: auto 30% 25%;
-    grid-template-areas:
-        "n h h h"
-        "n y . a"
-        "n f f f";
-
-    & > header {
-        grid-area: h;
-    }
-
-    & > nav {
-        grid-area: n;
-    }
-
-    & > main {
-        grid-area: y;
-    }
-
-    & > aside {
-        grid-area: a;
-    }
-
-    & > footer {
-        grid-area: f;
-    }
-`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,31 @@
 import React from "react"
-import Container from "../components/container"
-import Header from "../components/header"
-import Nav from "../components/nav"
-import Main from "../components/main"
-import Ad from "../components/ad"
-import Footer from "../components/footer"
+import Grid from "@material-ui/core/Grid"
+
+const h = window.innerHeight - document.documentElement.clientHeight
 
 export default function X(props) {
     return (
-        <Container>
-            <Header>
-                <h1>British Racing Green</h1>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </Header>
-            <Nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Cool</li>
-                </ul>
-            </Nav>
-            <Main></Main>
-            <Ad>
-                <a href="/">START YOUR FREE TRIAL!</a>
-            </Ad>
-            <Footer></Footer>
-        </Container>
+        <Grid container>
+            <Grid
+                style={{ backgroundColor: "orange", height: `${100 - h}vh` }}
+                item
+                xs={8}
+            >
+                LEFT
+            </Grid>
+            <Grid
+                container
+                style={{ backgroundColor: "#004225" }}
+                item
+                xs={4}
+                justify="center"
+            >
+                <Grid item xs={8}>
+                    <ul>
+                        <li>Home</li>
+                    </ul>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }

1da3372370f1ca3685c4d4a56369dd7bb32dd9be
jhamPac <cloud@kevia.me>
2020-04-19 08:51:07 -0700

    save point after content component

--- src/components/head.js
+++ src/components/head.js
@@ -1,11 +1,11 @@
 import React from "react"
 import styled from "styled-components"
 
-const C = styled.div`
+const H = styled.div`
     text-align: center;
     position: relative;
 `
 
-export default function Container(props) {
-    return <C>{props.children}</C>
+export default function Head(props) {
+    return <H>{props.children}</H>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,19 @@
-import React from "react"
-import Container from "../components/container"
+import React, { Fragment } from "react"
+import Head from "../components/head"
 import SubHead from "../components/subhead"
 import Header from "../components/header"
+import Content from "../components/content"
 
 export default function X(props) {
     return (
-        <Container>
-            <Header>Kevia's Street Journal</Header>
-            <SubHead>Phoenix, AZ - 00:00:00, 3000</SubHead>
-            <div>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </div>
-        </Container>
+        <>
+            <Head>
+                <Header>Kevia's Street Journal</Header>
+                <SubHead>Phoenix, AZ - 00:00:00, 3000</SubHead>
+            </Head>
+            <Content>
+                <div>asdfasdf</div>
+            </Content>
+        </>
     )
 }

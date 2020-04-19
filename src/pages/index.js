8074eb6aa8e72bb32471aa4ca7e31441292c7d73
jhamPac <cloud@kevia.me>
2020-04-19 10:38:52 -0700

    second column done

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,12 +20,54 @@ export default function X(props) {
                             <span>{`"Start Here"`}</span>
                             <span>This is the intro</span>
                             <p>
-                                Lorem Ipsum is simply dummy text of the printing
-                                and typesetting industry. Lorem Ipsum has been
-                                the industry's standard dummy text ever since
-                                the 1500s, when an unknown printer took a galley
-                                of type and scrambled it to make a type specimen
-                                book.
+                                Welcome to my random thoughts, aha moments,
+                                musings, rants and what not
+                            </p>
+                            <p>
+                                So any way. Let's start off with where else you
+                                can find me!
+                            </p>
+                            <p>
+                                <a
+                                    href="https://twitter.com/keviaDotMe"
+                                    target="_blank"
+                                >
+                                    Twitter
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href="https://github.com/jhamPac"
+                                    target="_blank"
+                                >
+                                    Github
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href="https://stackoverflow.com/users/3276646/jhampac"
+                                    target="_blank"
+                                >
+                                    Stackoverflow
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href="https://www.linkedin.com/in/kevia-cloud"
+                                    target="_blank"
+                                >
+                                    LinkedIn
+                                </a>
+                            </p>
+                            <p>
+                                Yeah, as you can see I work as a software
+                                engineer. Notice I said *I WORK* as a software
+                                engineer as opposed to *I'AM* a software
+                                engineer. I say that because my work doesn't
+                                define me. Isn't weird how we always introduce
+                                ourselves by our professions instead of with our
+                                hobbies and interests? It might be hard for some
+                                of us tho. Our profession is our hobby 👨🏽‍💻.
                             </p>
                         </Mercury>
                     </Column>
@@ -34,12 +76,12 @@ export default function X(props) {
                             <span>Yeezy Boost 350</span>
                             <span>***</span>
                             <p>
-                                Lorem Ipsum is simply dummy text of the printing
-                                and typesetting industry. Lorem Ipsum has been
-                                the industry's standard dummy text ever since
-                                the 1500s, when an unknown printer took a galley
-                                of type and scrambled it to make a type specimen
-                                book.
+                                I rate them 3 out of 5 stars. The styling is
+                                amazing but they feel a little "socky" after a
+                                while. In terms of comfort the adidas ultra
+                                boost are by far the best. But you can't deny
+                                the profile of these shoes. Very innovative and
+                                what the kids say these days... COOL.
                             </p>
                             <figure>
                                 <img src="https://i.pinimg.com/originals/ef/c0/00/efc0001ecad2dc93e4ed5e92f1ab814e.jpg" />
@@ -65,7 +107,7 @@ export default function X(props) {
                     </Column>
                     <Column>
                         <Mercury>
-                            <span>{`"Start Here"`}</span>
+                            <span>Other Stuff</span>
                             <span>This is the intro</span>
                             <p>
                                 Lorem Ipsum is simply dummy text of the printing

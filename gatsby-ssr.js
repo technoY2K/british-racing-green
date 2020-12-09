65516a0ecbef904c831b8dad44fce3bb71637bc5
jhamPac <cloud@kevia.me>
2020-12-09 08:59:46 -0700

    Remove unused import in browser and ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,6 @@
 import * as React from "react"
 import SEO from "./src/components/seo"
 
-import { wrapRootElement } from "./src/apollo/wrap-root-element"
-
 const wrapPageElement = ({ element, props }) => {
     return (
         <React.Fragment>
